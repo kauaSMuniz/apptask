f2f6c82f68e0926725f085ed2348a0759786c8d1
kaua <munizkaua555@gmail.com>
2025-03-17 12:35:34 -0300

    salvar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+
+//interface para criar um "objeto" com nossa tarefa
+interface Task{
+  id:number;
+  title:string;
+  completed:boolean;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [CommonModule,FormsModule,IonicModule],
 })
 export class HomePage {
+
+  task: Task[] =[];//inicia lista de tarefas vazia
+  newTask: string =''//nova tarefa (valor do campo)
+
+  addTask(){
+    if(this.newTask.trim()){
+
+      const newTaskobj:Task = {
+        id:Date.now(),
+        title:this.newTask.trim(),
+        completed: false
+      };
+      this.task.push(newTaskobj)
+
+      this.newTask = ''
+    }
+  }
+
   constructor() {}
 }
