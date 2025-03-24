730b4e5f87c0c3a3faa52f2ef9b2b8dbb79ea789
kaua <munizkaua555@gmail.com>
2025-03-24 12:35:36 -0300

    salvar

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,40 +1,76 @@
 import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton, IonList, IonLabel, IonIcon } from '@ionic/angular/standalone';
 
+//Função para adicionar o icone
 
-//interface para criar um "objeto" com nossa tarefa
-interface Task{
-  id:number;
-  title:string;
-  completed:boolean;
+import { addIcons } from 'ionicons';
+
+//Importar o icone
+
+import {ellipseOutline,checkmarkCircle } from 'ionicons/icons'
+
+//Interfaca para criar um "objeto" com nossa tarefa
+interface Task {
+  id: number;
+  title: string;
+  completed: boolean;
 }
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [CommonModule,FormsModule,IonicModule],
-})
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonButton, CommonModule, FormsModule,IonList, IonLabel, IonIcon],})
 export class HomePage {
 
-  task: Task[] =[];//inicia lista de tarefas vazia
-  newTask: string =''//nova tarefa (valor do campo)
+  tasks: Task[] = []; //Inicia lista de tarefas vazia
+  newTask: string = ''; //nova tarefa (valor do campo)
+  exibirConcluidos: boolean = true;
 
-  addTask(){
-    if(this.newTask.trim()){
+  addTask() {
+    if (this.newTask.trim()) {
 
-      const newTaskobj:Task = {
-        id:Date.now(),
-        title:this.newTask.trim(),
+      const newTaskObj: Task = { // Cria objeto Task
+        id: Date.now(),
+        title: this.newTask.trim(),
         completed: false
       };
-      this.task.push(newTaskobj)
+      this.tasks.push(newTaskObj);
 
-      this.newTask = ''
+      this.newTask = '';
     }
+
+
+    
   }
 
-  constructor() {}
+  concluir(taskId:number){
+    //Recupela da Lista de task
+    const task = this.tasks.find((task)=> task.id === taskId);
+  
+    //Marcar task como concluida/não concluida
+    if(task){
+      task.completed = !task.completed;
+    }
+
+    console.log(task);
+    
+
+  }
+
+
+  constructor() {
+
+    addIcons({ellipseOutline, checkmarkCircle});
+   }
+
+   mostrarOcultar(){
+    this.exibirConcluidos = !this.exibirConcluidos
+   }
+
+   get filteredTask(){
+    return this.tasks.filter((task) => this.exibirConcluidos || !task.completed)
+   }
 }
